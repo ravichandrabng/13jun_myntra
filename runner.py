c1715d9087aeb445f95f41660f7c6ae67b8b98e6
prishka <kspramodpython@gmail.com>
2020-05-30 23:20:20 +0530

    Fixed parallel execution

--- runner.py
+++ runner.py
@@ -12,14 +12,15 @@ class ReportPlugin:
     def pytest_sessionfinish(self):
         globals.ALLURE_REPORT = globals.ALLURE_REPORT + datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
         os.popen("allure generate " + globals.ALLURE_RESULTS + " --output " + globals.ALLURE_REPORT)
-        sleep(15)
-        helper.delete_all_files(globals.ALLURE_RESULTS)
 
 
-file_path = "./requirements.txt"
+'''file_path = "./requirements.txt"
 abs_path = os.path.abspath(file_path)
 command = "pip install -r " + abs_path
-os.system(command)
+os.system(command)'''
 
-args = ['--alluredir', globals.ALLURE_RESULTS]
+sleep(15)
+helper.delete_all_files(globals.ALLURE_RESULTS)
+
+args = ['-n', '2', '--alluredir', globals.ALLURE_RESULTS]
 pytest.main(args, plugins=[ReportPlugin()])
